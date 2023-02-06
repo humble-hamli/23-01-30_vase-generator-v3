79c94062a836bbc0614a0ca5838a95765471fae8
humble-hamli <cyril.truninger@icloud.com>
2023-02-06 11:27:53 +0100

    Vase Creator v3 Working in p5 JS soley

---
+++ 2D-Vase.js
@@ -0,0 +1,105 @@
+// 2D Vase
+let sketch1 = function (sketch) {
+  let cvs = cvs1;
+  console.log(cvs);
+  let cvsW = getCvsWidth(cvs);
+  let cvsH = getCvsWidth(cvs);
+
+  sketch.setup = function () {
+    console.log(cvs);
+    let canvas = sketch.createCanvas(cvsW, cvsH);
+    cvs.prepend(canvas.elt);
+    console.log(`Canvas 1 created size: ${cvsW} ${cvsW}`);
+  };
+
+  sketch.draw = function () {
+    let sliceCount = document.querySelector("#sliceCount").value;
+    let vg = vaseGuides;
+    
+
+    //for canvas 1
+    sketch.background(60);
+
+    if (gridButton.ariaPressed == "true") {
+      drawGrid();
+    }
+
+    if (guideShapeButton.ariaPressed == "true") {
+      drawGuideShape();
+    }
+
+    if (guideButton.ariaPressed == "true") {
+      drawGuides();
+    }
+
+    function drawGuides() {
+      
+      console.log("Vase Guides: ", vg)
+      // Draw Vase Guides
+      vg.map((d, i) => {
+        let centerW = cvsW / 2;
+        let x1 = centerW - ((cvsW / 100) * d.x) / 2;
+        let y1 = (cvsH / 100) * d.y;
+
+        let x2 = centerW + ((cvsW / 100) * d.x) / 2;
+        let y2 = (cvsH / 100) * d.y;
+        // console.log(d)
+        sketch.push();
+        sketch.strokeWeight(3);
+        sketch.stroke(255, 0, 255);
+        sketch.line(x1, y1, x2, y2);
+        sketch.pop();
+      });
+    }
+
+    function drawGuideShape() {
+      // Draw Vase Shape
+      sketch.push();
+      sketch.strokeWeight(1);
+      sketch.stroke(255, 0, 255);
+      sketch.fill(122, 0, 122);
+      sketch.beginShape();
+
+      vg.forEach((d, i) => {
+        let centerW = cvsW / 2;
+        let x1 = centerW - ((cvsW / 100) * d.x) / 2;
+        let y1 = (cvsH / 100) * d.y;
+        sketch.vertex(x1, y1);
+      });
+
+      vg
+        .slice(0)
+        .reverse()
+        .forEach((d, i) => {
+          let centerW = cvsW / 2;
+          let x2 = centerW + ((cvsW / 100) * d.x) / 2;
+          let y2 = (cvsH / 100) * d.y;
+          sketch.vertex(x2, y2);
+        });
+
+      sketch.endShape();
+      sketch.pop();
+    }
+
+    function drawGrid() {
+      // Draw Grid
+      sketch.push();
+      for (let y = 0; y < cvsH; y += cvsH / sliceCount) {
+        sketch.stroke(120);
+        sketch.line(0, y, cvsW, y);
+      }
+      // Draw center line
+      sketch.stroke(90);
+      sketch.line(cvsW / 2, 0, cvsW / 2, cvsH);
+      sketch.pop();
+    }
+  };
+
+  sketch.windowResized = function () {
+    let cvsW = getCvsWidth(cvs);
+    let cvsH = getCvsWidth(cvs);
+    sketch.resizeCanvas(cvsW, cvsH);
+  };
+};
+
+new p5(sketch1);
---
+++ Vase.js
@@ -0,0 +1,45 @@
+// Function for responsiv Canvases b
+function getCvsWidth(cvs) {
+  var cs = getComputedStyle(cvs);
+
+  var paddingX = parseFloat(cs.paddingLeft) + parseFloat(cs.paddingRight);
+  var paddingY = parseFloat(cs.paddingTop) + parseFloat(cs.paddingBottom);
+
+  var borderX =
+    parseFloat(cs.borderLeftWidth) + parseFloat(cs.borderRightWidth);
+  var borderY =
+    parseFloat(cs.borderTopWidth) + parseFloat(cs.borderBottomWidth);
+
+  // Element width and height minus padding and border
+  let elementWidth = cvs.offsetWidth - paddingX - borderX;
+  let elementHeight = cvs.offsetHeight - paddingY - borderY;
+
+  let cvsW = elementWidth;
+
+  return cvsW;
+}
+
+// Get Random Number
+function getRndNumber(min, max) {
+  return Math.random() * (max - min + 1) + min;
+}
+
+
+let vaseGuides = [
+  { id: "lip", x: getRndNumber(20, 80), y: 0 },
+  { id: "body", x: getRndNumber(20, 80), y: 80 },
+  { id: "foot", x: getRndNumber(20, 80), y: 100 },
+];
+
+console.log(reloadButton)
+
+reloadButton.onclick = (() => {
+    vaseGuides = [
+      { id: "lip", x: getRndNumber(20, 80), y: 0 },
+      { id: "body", x: getRndNumber(20, 80), y: 80 },
+      { id: "foot", x: getRndNumber(20, 80), y: 100 },
+    ];
+})
+
+
+console.log("Master Values: ", vaseGuides);
